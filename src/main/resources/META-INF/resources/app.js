9815666ca82c8d8151e2f232088d7c9beacfe064
Sergio Canales <sergio.canales.e@gmail.com>
2024-04-19 15:09:45 -0400

    demo version ready
    Works, simple and ugly

--- src/main/resources/META-INF/resources/app.js
+++ src/main/resources/META-INF/resources/app.js
@@ -1,7 +1,7 @@
 // src/main/resources/META-INF/resources/app.js
 async function fetchUsers() {
     try {
-        const response = await fetch('http://localhost:8080/users/all');
+        const response = await fetch('http://localhost:8080/api/users/all');
         if (!response.ok) throw new Error('Service is unavailable');
         const users = await response.json();
         const userListHTML = users.map(user => `<li>${user.name}</li>`).join('');
@@ -12,16 +12,6 @@ async function fetchUsers() {
         document.getElementById('error').style.display = 'block';
     }
 }
-const eventSource = new EventSource('http://localhost:8080/stream');
-eventSource.onmessage = function(event) {
-    console.log('Received message:', event.data);
-    // Update the UI or take other actions based on the event data
-};
-
-eventSource.onerror = function() {
-    console.log('Error receiving SSE');
-    // Handle errors or try to reconnect
-};
 
 // Initial call and set to retry every 5 seconds
 fetchUsers();
