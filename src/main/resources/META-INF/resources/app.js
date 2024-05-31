c67c34327a371a02518464e08fda62dccbb304d4
Sergio Canales <sergio.canales.e@gmail.com>
2024-05-30 20:30:28 -0400

    env para despliegue en k8s

--- src/main/resources/META-INF/resources/app.js
+++ src/main/resources/META-INF/resources/app.js
@@ -1,7 +1,7 @@
 // src/main/resources/META-INF/resources/app.js
-async function fetchUsers() {
+async function fetchUsers(host,port) {
     try {
-        const response = await fetch('http://localhost:8080/api/users/all');
+        const response = await fetch("https://"+host+":"+port+"/api/users/all");
         if (!response.ok) throw new Error('Service is unavailable');
         const users = await response.json();
         const userListHTML = users.map(user => `<li>${user.name}</li>`).join('');
@@ -13,6 +13,35 @@ async function fetchUsers() {
     }
 }
 
-// Initial call and set to retry every 5 seconds
-fetchUsers();
-setInterval(fetchUsers, 5000);
+function loadEnv(envString) {
+    const envVariables = envString.trim().split('\n');
+    const env = {};
+
+    envVariables.forEach(variable => {
+      const [key, value] = variable.split('=');
+      if (key && value) {
+        env[key.trim()] = value.trim();
+      }
+    });
+
+    return env;
+  }
+
+  fetch('env')
+  .then(response => response.text())
+  .then(async envFileContent => {
+    console.log(envFileContent);
+    const env = loadEnv(envFileContent);
+
+    const port = env.PORT;
+    console.log(port);
+    const host = env.HOST;
+    console.log(host);
+
+    // Initial call and set to retry every 5 seconds
+    fetchUsers(host,port);
+    setInterval(fetchUsers, 5000);
+
+  })
+  .catch(error => console.error('Error loading env file:', error));
+
